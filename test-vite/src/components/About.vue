100b9975c9a9649dc3573177e3d916ae63b74ceb
zhangjianyu <zhangjianyu@weimiao.cn>
2021-07-21 14:14:01 +0800

    init

---
+++ test-vite/src/components/About.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="tips">
+    {{ count }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent ,computed} from "vue";
+import { useStore } from "vuex";
+ import { key } from '../store'
+
+export default defineComponent({
+  name: "About",
+  data() {
+    return {
+      msg: "Hello Vue 3.0 + Vite!",
+    };
+  },
+  setup() {
+    const store =  useStore(key)
+    const count = computed(() => store.state.count)
+    return {
+      count
+    }
+  },
+});
+</script>
---
+++ test-vite/src/store/index.ts
@@ -0,0 +1,24 @@
+import { InjectionKey } from 'vue'
+import { createStore, Store } from 'vuex'
+
+export interface State {
+  count: number,
+  msg:String,
+}
+
+export const key: InjectionKey<Store<State>> = Symbol()
+
+export const store = createStore<State>({
+  state() {
+    return {
+      count: 0,
+      msg:'23232'
+    }
+  },
+  mutations: {
+    increment(state) {
+      console.log(state,"=============")
+      state.count++
+    }
+  }
+})
