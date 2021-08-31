b637bb7ae528d0ce648cc0d7a511bfcc2a269677
zhangjianyu <zhangjianyu@weimiao.cn>
2021-08-31 16:41:12 +0800

    update

---
+++ mp/client/src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view className='index'>
+    <Login />
+  </view>
+</template>
+
+<script>
+import './index.scss'
+import Login from '../../components/login/index.weapp'
+
+export default {
+  name: 'Index',
+  components: {
+    Login
+  }
+}
+</script>
