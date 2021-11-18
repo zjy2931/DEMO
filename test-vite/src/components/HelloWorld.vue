299b2a3b8e497599658e90a244ccb154bad889f5
zjy2931 <479247526@qq.com>
2021-11-18 16:03:55 +0800

    change

--- test-vite/src/components/HelloWorld.vue
+++ test-vite/src/components/HelloWorld.vue
@@ -1,29 +1,30 @@
 <template>
   <h1>{{ msg }}</h1>
-  <button @click="inCrement"> count is: </button>
+  <button @click="inCrement">count is:</button>
   <p>{{ count }}</p>
+  <p class="d">{{count*3}}</p>
 </template>
 
-<script>
-  import { defineComponent, computed } from 'vue'
-  import { useStore } from 'vuex'
-  import { key } from '../store'
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import { useStore } from 'vuex'
+import { key } from '../store'
 
-  export default defineComponent({
-    name: 'HelloWorld',
-    props: {
-      msg: {
-        type: String,
-        default: ''
-      }
-    },
-    setup() {
-      const store = useStore(key)
-      const count = computed(() => store.state.count)
-      return {
-        count,
-        inCrement: () => store.commit('increment')
-      }
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      default: ''
     }
-  })
+  },
+  setup() {
+    const store = useStore(key)
+    const count = computed(() => store.state.count)
+    return {
+      count,
+      inCrement: () => store.commit('increment')
+    }
+  }
+})
 </script>
--- test-vite/src/url.ts
+++ test-vite/src/url.ts
@@ -1,8 +1,8 @@
-function utf8_to_b64( str ) {
+function utf8_to_b64( str:string ):string {
     return window.btoa(unescape(encodeURIComponent( str )));
   }
   
-  function b64_to_utf8( str ) {
+  function b64_to_utf8( str:string ):string {
     return decodeURIComponent(escape(window.atob( str )));
   }
   
